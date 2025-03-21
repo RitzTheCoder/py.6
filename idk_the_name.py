c3bd0f3edb0c9bbdf1e892714e184d90dd03cbce
RitzTheCoder <ritzthecoder@gmail.com>
2025-03-21 18:03:14 +0530

    Add files via upload

---
+++ idk_the_name.py
@@ -0,0 +1,24 @@
+a=10
+b=12
+c=12
+print(a != b)
+print(b !== c)
+
+a="JavaScript"
+b="Python"
+if a != b :
+    print(a,"and",b "are different Programing Languages")
+
+a=4
+b=5
+if(a == 1)!=(b == 5):
+    print("HELLO GUYSSS ")
+
+a = int(input("ENTER A NUM U FELLA"))
+if a%2 !=0 :
+    print(a,"IS NOT A EVEN NUM HEHEHE")
+
+
+
+
+        
