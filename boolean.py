ea4cf48a09cdf2f0b52d50b4c19e7bbdc02578aa
RitzTheCoder <ritzthecoder@gmail.com>
2025-03-21 17:42:21 +0530

    Add files via upload

---
+++ boolean.py
@@ -0,0 +1,21 @@
+a = 10
+b = 12
+c = 0
+if a and b and c:
+    print("All the numbers in boolen are true")
+else:
+    print("Atleast one of the boolen num is false") 
+
+a=10
+b=-10
+c=0
+if a > 0 or b > 0:
+   print("Either of them is false")
+else:
+    print("No number is greater than 0 ")   
+   
+if b > 0 or c > 0:
+   print("Either of them is false")
+else:
+    print("No number is greater than 0 ")   
+      
